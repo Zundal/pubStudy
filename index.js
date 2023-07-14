fe8dfca0a7dfff60c9b6862bcca05326d57fba6b
Jeongsik An <ajs15010120@gmail.com>
2023-07-14 18:18:39 +0900

    호스트명 : 정식 맥북 backup: 2023-07-14 18:18:39

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 window.addEventListener('DOMContentLoaded', function() {
-        const nav = document.getElementById('nav');
+    const footer = document.getElementById('footer');
+    fetch('./footer.html')
+    .then(response => response.text())
+    .then(html => {
+        footer.innerHTML = html;
+    });
+
+    const nav = document.getElementById('nav');
     fetch('./navigator.html')
     .then(response => response.text())
     .then(html => {
